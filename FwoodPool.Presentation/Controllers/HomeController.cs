ddc93b57d718b2079086ed84160f4a68bf662870
danieldutton <danielbdutton@hotmail.co.uk>
2014-06-04 11:08:20 +0100

    various
    1.  Created Home controller
    2.  Created corresponding skeleton views
    3.  Added unit test project
    4.  Completed unit tests for HomeController

---
+++ FwoodPool.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace FwoodPool.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Rules()
+        {
+            return View();
+        }
+
+        public ActionResult Constitution()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FwoodPool.UnitTests/Presentation/HomeController_Should.cs
@@ -0,0 +1,56 @@
+﻿using FwoodPool.Presentation.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace FwoodPool.UnitTests.Presentation
+{
+    [TestFixture]
+    public class HomeController_Should
+    {
+        private HomeController _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new HomeController();
+        }
+
+        [Test]
+        public void Index_ReturnTheCorrectView()
+        {
+            var viewResult = _sut.Index() as ViewResult;
+
+            Assert.AreEqual(string.Empty, viewResult.ViewName);
+        }
+
+        [Test]
+        public void Rules_ReturnTheCorrectView()
+        {
+            var viewResult = _sut.Rules() as ViewResult;
+
+            Assert.AreEqual(string.Empty, viewResult.ViewName);
+        }
+
+        [Test]
+        public void Constitution_ReturnTheCorrectView()
+        {
+            var viewResult = _sut.Constitution() as ViewResult;
+
+            Assert.AreEqual(string.Empty, viewResult.ViewName);
+        }
+
+        [Test]
+        public void Contact_ReturnTheCorrectView()
+        {
+            var viewResult = _sut.Contact() as ViewResult;
+
+            Assert.AreEqual(string.Empty, viewResult.ViewName);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _sut = null;
+        }
+    }
+}
